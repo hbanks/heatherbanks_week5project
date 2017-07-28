be4a46428b292c7397a8f687b2367dae0208d9d4
Tej Kahlon <tejpartapsingh91@gmail.com>
2017-07-28 13:44:34 -0400

    Changed script terminology to ES6+.

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,205 +1,155 @@
-// create a blank namespace
+//==========
+// NAMESPACE
+//==========
 var pastryApp = {};
 
-pastryApp.clientId = 'WNANNEHBRREEDHVD4IVAOCHGT4SHT1Q4ZM0FWM3IQPEA2NNB';
+//====
+// API
+//====
+pastryApp.clientId = "WNANNEHBRREEDHVD4IVAOCHGT4SHT1Q4ZM0FWM3IQPEA2NNB";
+pastryApp.clientSecret = "0QV0SFDCT3VULM5RBGCLQYRTLNC2PWG4TPUOR0WXQX3R5SPF";
 
-pastryApp.clientSecret = '0QV0SFDCT3VULM5RBGCLQYRTLNC2PWG4TPUOR0WXQX3R5SPF';
-
-pastryApp.init = function() {
-	$('.search').on('submit', function (e) {
+//=========
+// APP INIT
+//=========
+pastryApp.init = () => {	
+	$(".search").on("submit", function (e) {
 		e.preventDefault();
-		pastryApp.place = $('.place').val();
-		console.log('we should find treats in ' + pastryApp.place);
+		pastryApp.place = $(".place").val();
 		pastryApp.getLocation(pastryApp.place);
-		$('.place').val('');
-
-		//clear out old locations
-		$('.locationSuccess').html('');
-
-		var divSuccess = $('<div>').addClass('locationSuccess');
-		$('.setLocation').append(divSuccess);
-		var locationSuccess = $('<p>').text('Looking for treats in ' + pastryApp.place + ', now...');
+		$(".place").val("");
+		$(".locationSuccess").html("");
+		var divSuccess = $("<div>").addClass("locationSuccess");
+		$(".setLocation").append(divSuccess);
+		var locationSuccess = $("<p>").text("Looking for treats in " + pastryApp.place + ", now...");
 		divSuccess.append(locationSuccess);
+		console.log("we should find treats in " + pastryApp.place);
 	});
-
-	$('a.doughnut').on('click', function(e) {
+	$("a.doughnut").on("click", function(e) {
 		e.preventDefault();
-		var pastry = 'doughnut';
-		console.log('we want ' + pastry);
-		// pass the value of doughtnut to getBakery();
+		var pastry = "doughnut";
 		pastryApp.getBakery(pastry);
+		console.log("we want " + pastry);
 	});
-
-	$('a.cupcake').on('click', function(e) {
+	$("a.cupcake").on("click", function(e) {
 		e.preventDefault();
-		var pastry = 'cupcake';
-		console.log('we want ' + pastry);
-		// pass the value of cupcake to getBakery();
+		var pastry = "cupcake";
 		pastryApp.getBakery(pastry);
+		console.log("we want " + pastry);
 	});
-
-	$('a.vegan').on('click', function(e) {
+	$("a.vegan").on("click", function(e) {
 		e.preventDefault();
-		var pastry = 'vegan bakery';
-		console.log('we want ' + pastry);
-		// pass the value of vegan bakery to getBakery();
+		var pastry = "vegan bakery";
 		pastryApp.getBakery(pastry);
+		console.log("we want " + pastry);
 	});
-} // end init
+};
 
-//=========
-//ajax call
-//=========
-
-pastryApp.getLocation = function(place) {
+//=====
+// AJAX
+//=====
+pastryApp.getLocation = place => {
 	$.ajax({
-		url : 'https://api.foursquare.com/v2/venues/explore?&v=20130815',
-		type : 'GET',
-		dataType : 'jsonp',
+		url : "https://api.foursquare.com/v2/venues/explore?&v=20130815",
+		type : "GET",
+		dataType : "jsonp",
 		data : {
 			client_id : pastryApp.clientId,
 			client_secret : pastryApp.clientSecret,
-			format : 'jsonp',
+			format : "jsonp",
 			near : pastryApp.place
 		},
 		success : function (response) {
 			console.log(response);
 		}
+	});
+};
 
-	}) //end ajax
-}
-
-//=======================
-//get & display functions
-//=======================
-
-pastryApp.getBakery = function(pastry, response) {
+//==========
+// FUNCTIONS
+//==========
+pastryApp.getBakery = (pastry, response) => {
 	$.ajax({
-		url : 'https://api.foursquare.com/v2/venues/explore?&v=20130815',
-		type : 'GET',
-		dataType : 'jsonp',
+		url : "https://api.foursquare.com/v2/venues/explore?&v=20130815",
+		type : "GET",
+		dataType : "jsonp",
 		data : {
 			client_id : pastryApp.clientId,
 			client_secret : pastryApp.clientSecret,
-			format : 'jsonp',
+			format : "jsonp",
 			near : pastryApp.place,
-			venuePhotos : '1',
+			venuePhotos : "1",
 			query : pastry
 		},
 		success : function(result) {
 			console.log(result);
 			pastryApp.displayBakery(result);
-		} // end success
-	}); //end ajax
-}; // end getBakery();
-
-pastryApp.displayBakery = function(result) {
-	//clear out old bakeries
-	$('.bakeries').html('');
-	
-	var bakeries = result.response.groups[0].items;
-	console.log(bakeries);
-	
-	//=========
-	// for loop
-	//=========
-	for(var i = 0; i < bakeries.length; i++) {
-		var div = $('<div>').addClass('bakery clearfix');
-		var venues = bakeries[i].venue;
-		
-		// get bakery information
-		var h2 = $('<h2>').text(venues.name);
-		var locations = $('<p>').addClass('address').html('<i class="fa fa-home"></i>' + venues.location.address + ', ' + venues.location.city);
-		
-		// open links in new tab
-		$('a.target-blank').attr('target','_blank');
-		
-		// selecting the photo
-		var photoPrefix = venues.photos.groups[0].items[0];
-		var photo = $('<img>').attr('src',photoPrefix.prefix + photoPrefix.height + photoPrefix.suffix);
-
-		// linking to the Foursquare Page
-		var addrPrefix = "https://foursquare.com/v/" + venues.id;
-		var addr = $('<a>').addClass('foursquareAddr target-blank').attr('href', addrPrefix).html('<i class="fa fa-foursquare"></i> Find us on Foursquare!');
-		
-		div.append(h2,photo,location,addr)
-
-		//======================
-		// results if statements
-		//======================
-
-		// if the result has a phone number, do this
-		if (pastryApp.hasPhone(venues.contact.formattedPhone)) {
-			var contactPhone = $('<p>').addClass('contactPhone').html('<i class="fa fa-phone"></i>' + venues.contact.formattedPhone);
-			div.append(h2,photo,locations,contactPhone,addr)
-		} else {
-			div.append(h2,photo,locations,addr)
 		}
+	});
+};
 
-		// if the result has a twitter handle, create the contactTwitter variable and append to the html with everything
-		var twitterPrefix = 'https://twitter.com/';
-		if (pastryApp.hasTwitter(venues.contact.twitter)) {
-			var contactTwitter = $('<a>').addClass('contactTwitter target-blank').attr('href', twitterPrefix + venues.contact.twitter).html('<i class="fa fa-twitter"></i>' + '@' + venues.contact.twitter);
-			div.append(contactTwitter,addr);
-		// if it does not have a twitter handle, do not create the variable, and append the rest without the twitter variable!	
+pastryApp.displayBakery = result => {	
+	$(".bakeries").html("");	
+	var bakeries = result.response.groups[0].items;
+	for(var i = 0; i < bakeries.length; i++) {
+		var div = $("<div>").addClass("bakery clearfix");
+		var venues = bakeries[i].venue;
+		var h2 = $("<h2>").text(venues.name);
+		var locations = $("<p>").addClass("address").html("<i class=\"fa fa-home\"></i>" + venues.location.address + ", " + venues.location.city);
+		$("a.target-blank").attr("target","_blank");
+		var photoPrefix = venues.photos.groups[0].items[0];
+		var photo = $("<img>").attr("src",photoPrefix.prefix + photoPrefix.height + photoPrefix.suffix);
+		var addrPrefix = "https://foursquare.com/v/" + venues.id;
+		var addr = $("<a>").addClass("foursquareAddr target-blank").attr("href", addrPrefix).html("<i class=\"fa fa-foursquare\"></i> Find us on Foursquare!");
+		div.append(h2,photo,location,addr);
+		if (pastryApp.hasPhone(venues.contact.formattedPhone)) {
+			var contactPhone = $("<p>").addClass("contactPhone").html("<i class=\"fa fa-phone\"></i>" + venues.contact.formattedPhone);
+			div.append(h2,photo,locations,contactPhone,addr);
 		} else {
-			// div.append(h2,photo,locations,addr);
-		};
-
-		// if the result has a website, do this, if not, do that
+			div.append(h2,photo,locations,addr);
+		}
+		var twitterPrefix = "https://twitter.com/";
+		if (pastryApp.hasTwitter(venues.contact.twitter)) {
+			var contactTwitter = $("<a>").addClass("contactTwitter target-blank").attr("href", twitterPrefix + venues.contact.twitter).html("<i class=\"fa fa-twitter\"></i>" + "@" + venues.contact.twitter);
+			div.append(contactTwitter,addr);
+		}
 		if (pastryApp.hasWebsite(venues.url)) {
-			var url = venues.url;
-			var newUrl = venues.url.replace(/https?:\/\//, '');
-			console.log(newUrl);
-
-			if(venues.url.match(/https?:\/\//)) {
-				console.log('matches')
-				} else {
-					console.log('doesnt match')
-				}
-			
-			var website = $('<a>').addClass('websiteURL target-blank').attr('href',venues.url).html('<i class="fa fa-globe"></i>' + newUrl);
-			div.append(website,addr)
-		}	
-		
-		
-		// adding the div to the html
-		$('.bakeries').append(div);
-	} // end for loop
-} // end displayBakery
-
-//==============
-//has statements
-//==============
-
-pastryApp.hasPhone = function(phoneProperty) {
-	if(!phoneProperty) {
-		return false;
-	} else {
-		return true
-	}
-}
-
-pastryApp.hasWebsite = function(websiteProperty) {
-	if(!websiteProperty) {
-		return false;
-	} else {
-		return true
+			var newUrl = venues.url.replace(/https?:\/\//, "");
+			var website = $("<a>").addClass("websiteURL target-blank").attr("href",venues.url).html("<i class=\"fa fa-globe\"></i>" + newUrl);
+			div.append(website,addr);
+		}
+		$(".bakeries").append(div);
 	}
 };
 
-pastryApp.hasTwitter = function(twitterProperty){
+//===========
+// CONDITIONS
+//===========
+pastryApp.hasPhone = phoneProperty => {
+	if(!phoneProperty) {
+		return false;
+	} else {
+		return true;
+	}
+};
+pastryApp.hasWebsite = websiteProperty => {
+	if(!websiteProperty) {
+		return false;
+	} else {
+		return true;
+	}
+};
+pastryApp.hasTwitter = twitterProperty => {
 	if (!twitterProperty) {
 		return false;
 	} else {
-       return true
-    }
-}
+		return true;
+	}
+};
 
-//==============
-//document ready
-//==============
-
+//===============
+// DOCUMENT READY
+//===============
 $(function() {
 	pastryApp.init();
 });
